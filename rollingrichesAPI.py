6de94c4b42067d6e7e9ba86fb89d86ec7295638c
DrakeHooks <52050841+DrakeHooks@users.noreply.github.com>
2025-06-28 04:49:40 -0400

    Update rollingrichesAPI.py
    screenshot on auth timeout

--- rollingrichesAPI.py
+++ rollingrichesAPI.py
@@ -1,16 +1,18 @@
 # Drake Hooks
-# Casino Claim
+# Casino Claim 2
 # Rolling Riches API
 
 import re
 import os
 import asyncio
+import discord
 from dotenv import load_dotenv
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+
 
 # Load environment variables from the .env file
 load_dotenv()
@@ -27,37 +29,45 @@ async def rolling_riches_casino(ctx, driver, channel):
         else:
             await channel.send("Rolling Riches credentials not found in environment variables.")
             return
-
         # Get the URL and allow page to load
-        driver.get("https://rollingriches.com/login")
-        await asyncio.sleep(10)
-        try:
-            email_input = WebDriverWait(driver, 10).until(
+        driver.get("https://rollingriches.com")
+        await asyncio.sleep(5)
+        login_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "/html/body/app-root/app-main-header/div/div/div/div/header/div[2]/div/button[1]"))
+        )
+        login_button.click()
+        await asyncio.sleep(90)
+        await asyncio.sleep(5)
+        email_input = WebDriverWait(driver, 5).until(
                 EC.presence_of_element_located((By.ID, "email"))
             )
-            login_button_present = True
-        except TimeoutException:
-            login_button_present = False
-
-        # If login button is found, perform login with credentials
-        if login_button_present:
-            email_input.send_keys(username)
-            password_input = driver.find_element(By.ID, "password")
-            password_input.send_keys(password)
-            password_input.send_keys(Keys.ENTER)
-            await asyncio.sleep(5)  # Wait for login process to complete
-        else:
-            # If no login button is found, assume already logged in
-            await channel.send("Rolling Riches session active, proceeding without login.")
-
+        email_input.send_keys(username)
+        await asyncio.sleep(5)  # Wait for email
+        password_input = driver.find_element(By.ID, "password")
+        password_input.send_keys(password)
+        password_input.send_keys(Keys.ENTER)
+        await asyncio.sleep(40)  # Wait for login process to complete
         # Now proceed with claiming the bonus
         await claim_rolling_riches_bonus(ctx, driver, channel)
-
     except TimeoutException as e:
+        screenshot_path = "1rr_screenshot.png"
+        driver.save_screenshot(screenshot_path)
+        await channel.send("Rolling Riches Authentication timed out, will try again later.", 
+                       file=discord.File(screenshot_path))
+        os.remove(screenshot_path)
         print(f"Login timeout: {e}")
         await channel.send("Login to Rolling Riches failed.")
     except Exception as e:
         print(f"Error in rolling_riches_casino")
+
+
+
+
+
+
+
+
+
 
 
 # Function to claim the 6-hour bonus
@@ -83,7 +93,7 @@ async def claim_rolling_riches_bonus(ctx, driver, channel):
     except Exception as e:
         print(f"Error: {str(e)}")
     finally:
-        await asyncio.sleep(10)
+        await asyncio.sleep(4)
         print("Checking for countdown element.")
         
         # List of possible XPaths for the countdown element
