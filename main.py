da86dcbf294ae985bc6b9f9692cd2d98c01adf1c
DrakeHooks <52050841+DrakeHooks@users.noreply.github.com>
2025-07-10 02:29:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ options.add_argument('--no-sandbox')
 user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"
 options.add_argument(f"--user-agent={user_agent}")
 
-#IMPORTANT. YOU MAY WANT TO IMPORT A CUSTOM USER DATA DIRECTORY FOR CAPTCHA SOLVING EXTENSIONS. UNCOMMENT THIS OUT AND IN THE DOCKERFILE FOR THIS
+#IMPORTANT. YOU MAY WANT TO IMPORT A CUSTOM USER DATA DIRECTORY FOR CAPTCHA SOLVING EXTENSIONS. UNCOMMENT THIS OUT HERE AND IN THE DOCKERFILE.
 
 # user_data_dir = "/temp/google-chrome/" 
 # Change path for Linux environment
