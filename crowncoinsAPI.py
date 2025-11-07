7b51536797b14b773cc8a68c284d30f3900a4332
DrakeHooks <52050841+DrakeHooks@users.noreply.github.com>
2025-11-07 02:52:07 -0500

    Update crowncoinsAPI.py

--- crowncoinsAPI.py
+++ crowncoinsAPI.py
@@ -35,7 +35,7 @@ async def auth_crown_google(driver, bot, ctx, channel):
 
         try:
             login_btn = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div/div[1]/div[1]/div[2]/button[2]"))
+                EC.element_to_be_clickable((By.ID, "lobby-login-btn"))
             ) 
             login_btn.click()
 
